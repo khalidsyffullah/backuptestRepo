823c4802c1c93c1dd5f1bbc756855c127ff314cd
Khalid Syffullah <47054320+khalidsyffullah@users.noreply.github.com>
2024-06-04 15:18:29 +0600

    Auto backup

---
+++ woo-pathao-order-check.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Plugin Name:       Woo pathao order check
+ * Plugin URI:        https://wordpress.org/plugins/woo-pathao-order-check/
+ * Description:       Check a customer's order receiving success rate
+ * Version:           1.0.0
+ * Requires at least: 6.4
+ * Requires PHP:      7.4
+ * Author:            NAFS Tech
+ * Author URI:        https://nafs.tech/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://nafs.tech/woo-pathao-order-check/updates
+ * Text Domain:       woo-pathao-order-check
+ * Domain Path:       /languages
+ * Requires Plugins:  woocommerce
+ */
+
+
+ 
