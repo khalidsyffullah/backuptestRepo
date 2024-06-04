3b6cb956758df7ea8af1334fe56d5c365d96b27e
Khalid Syffullah <47054320+khalidsyffullah@users.noreply.github.com>
2024-06-04 15:18:27 +0600

    Auto backup

---
+++ includes (copy)/templates/woo-pathao-order-data-modal.php
@@ -0,0 +1,24 @@
+<?php
+// <!-- woo-pathao-order-data-modal.php -->
+function woo_pathao_add_order_data_modal($order)
+{
+?>
+
+  <div id="pathao-response-modal" class="modal">
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <h3 class="pathao-response-data-header">Customer Report</h3>
+      <hr style="height:1px;border-width:0;color:gray;background-color:#f6f6f6">
+      <div class="pathao-response-data-section">
+        <div id="pathao-response-data">
+
+        </div>
+        <div id="pathao-dataBar">
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<?php
+}
